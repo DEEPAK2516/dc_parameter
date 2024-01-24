ded9e0d254a98cdd6428e6a919e10603ef875465
DEEPKA RAJ G <111234076+DEEPAK2516@users.noreply.github.com>
2024-01-24 22:46:40 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    function fetchData() {
+        fetch("https://api.apispreadsheets.com/data/P1V2Fx47xjvHy2pH/")
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Failed to fetch data');
+                }
+            })
+            .then(data => {
+                const lastData = data["data"][data["data"].length - 1];
+                displayData(lastData);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                updateValues("Error fetching data", "Error fetching data", "Error fetching data");
+            });
+    }
+
+    function displayData(item) {
+        updateValues(item.voltage, item.current, item.power);
+    }
+
+    function updateValues(voltage, current, power) {
+        document.getElementById("voltage-value").textContent = voltage;
+        document.getElementById("current-value").textContent = current;
+        document.getElementById("power-value").textContent = power;
+    }
+
+    // Fetch data initially
+    fetchData();
+
+    // Fetch data every 5 seconds (adjust the interval as needed)
+    setInterval(fetchData, 5000);
+});
